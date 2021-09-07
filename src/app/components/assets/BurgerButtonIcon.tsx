5369a9d325001a8311bb45af4993033034dfbe42
Seborider <mayer.sebobo@gmail.com>
2021-09-07 14:04:47 +0200

    ⚰️  Removed wrong svg fills

--- src/app/components/assets/BurgerButtonIcon.tsx
+++ src/app/components/assets/BurgerButtonIcon.tsx
@@ -12,9 +12,9 @@ export default function BurgerButtonIcon(
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <rect y="8" width="43" height="2" rx="1" fill="#284B63" />
+      <rect y="8" width="43" height="2" rx="1" />
       <path d="M0 17C0 16.4477 0.447715 16 1 16H42C42.5523 16 43 16.4477 43 17C43 17.5523 42.5523 18 42 18H1C0.447716 18 0 17.5523 0 17Z" />
-      <rect width="43" height="2" rx="1" fill="#284B63" />
+      <rect width="43" height="2" rx="1" />
     </svg>
   );
 }
