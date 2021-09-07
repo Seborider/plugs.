e18267de9a8ea232eb9501ebb6183d477f3aabcd
Seborider <mayer.sebobo@gmail.com>
2021-09-08 00:10:38 +0200

    🏷️  Add types for Logo

--- src/app/components/Logo/Logo.tsx
+++ src/app/components/Logo/Logo.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import LogoIcon from '../assets/LogoIcon';
 import styles from './Logo.module.css';
 
-export default function Logo(): JSX.Element {
+type LogoProps = {
+  className?: string;
+};
+
+export default function Logo({ className }: LogoProps): JSX.Element {
   return <LogoIcon className={styles.logo} fill={'var(--primary-accent)'} />;
 }
