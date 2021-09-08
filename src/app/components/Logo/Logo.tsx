9486a627a3d2920b6badf4f4f20af92e1385114f
Seborider <mayer.sebobo@gmail.com>
2021-09-08 17:24:14 +0200

    🔥  Revome LogoProps

--- src/app/components/Logo/Logo.tsx
+++ src/app/components/Logo/Logo.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import LogoIcon from '../assets/LogoIcon';
 import styles from './Logo.module.css';
 
-type LogoProps = {
-  className?: string;
-};
-
-export default function Logo({ className }: LogoProps): JSX.Element {
+export default function Logo(): JSX.Element {
   return <LogoIcon className={styles.logo} fill={'var(--primary-accent)'} />;
 }
