acb17c8ab29d12bcfef012733cdd2de99e275553
Seborider <mayer.sebobo@gmail.com>
2021-09-09 14:42:45 +0200

    🐛  Put component in html element

--- src/app/components/Logo/Logo.tsx
+++ src/app/components/Logo/Logo.tsx
@@ -3,5 +3,9 @@ import LogoIcon from '../assets/LogoIcon';
 import styles from './Logo.module.css';
 
 export default function Logo(): JSX.Element {
-  return <LogoIcon className={styles.logo} fill={'var(--primary-accent)'} />;
+  return (
+    <div className={styles.logo}>
+      <LogoIcon />
+    </div>
+  );
 }
