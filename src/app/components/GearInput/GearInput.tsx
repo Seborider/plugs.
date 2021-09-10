7ebbc3cefb05fcce85f82a6afb74abcb3767033b
Seborider <mayer.sebobo@gmail.com>
2021-09-10 19:03:05 +0200

    🏷️  Add types and onClick functionality

--- src/app/components/GearInput/GearInput.tsx
+++ src/app/components/GearInput/GearInput.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './GearInput.module.css';
 
-// export type GearInputProps = {};
+export type GearInputProps = {
+  placeholder:
+    | 'Output to'
+    | 'Input from'
+    | 'MIDI Out to'
+    | 'MIDI In from'
+    | 'USB Port';
+  onClick: (type: boolean) => void;
+};
 
-export default function GearInput(): JSX.Element {
+export default function GearInput({
+  placeholder,
+}: GearInputProps): JSX.Element {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <section className={style.container}>
       <label className={style.switch}>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          onClick={() => {
+            setToggle(!toggle);
+            console.log(toggle);
+          }}
+        />
         <span className={`${style.slider} ${style.round}`}></span>
       </label>
 
-      <form>
-        <input type="text" placeholder="asd" className={style.input} />
+      <form className={style.formWrapper}>
+        <input type="text" placeholder={placeholder} className={style.input} />
       </form>
     </section>
   );
