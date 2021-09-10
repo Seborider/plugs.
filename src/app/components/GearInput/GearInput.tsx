ec4fb21b46fdb5c057a10bc0cc67077c749cc5a7
Seborider <mayer.sebobo@gmail.com>
2021-09-10 16:49:08 +0200

    🎨  Add form

--- src/app/components/GearInput/GearInput.tsx
+++ src/app/components/GearInput/GearInput.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import Typography from '../Typography/Typography';
 import style from './GearInput.module.css';
 
 // export type GearInputProps = {};
 
 export default function GearInput(): JSX.Element {
   return (
-    <section>
-      <div>
-        <label className={style.switch}>
-          <input type="checkbox" />
-          <span className={`${style.slider} ${style.round}`}></span>
-        </label>
-      </div>
+    <section className={style.container}>
+      <label className={style.switch}>
+        <input type="checkbox" />
+        <span className={`${style.slider} ${style.round}`}></span>
+      </label>
+
+      <form>
+        <input type="text" placeholder="asd" className={style.input} />
+      </form>
     </section>
   );
 }
