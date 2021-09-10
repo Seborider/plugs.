4737fa6d78682059eb9986f4dd5b291c35a0b876
Seborider <mayer.sebobo@gmail.com>
2021-09-10 16:49:53 +0200

    💄  Set default background

--- src/app/components/GearInput/GearInput.stories.tsx
+++ src/app/components/GearInput/GearInput.stories.tsx
@@ -4,6 +4,12 @@ import GearInput from './GearInput';
 export default {
   title: 'Component/GearInput',
   component: GearInput,
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+    layout: '!padded',
+  },
 };
 
 export const AddGear = (): JSX.Element => <GearInput />;
