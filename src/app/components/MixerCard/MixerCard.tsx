319a9e35642da28a432cb388858983e973fb91e7
Seborider <mayer.sebobo@gmail.com>
2021-09-13 16:37:57 +0200

    🔥  Remove unnecessary divs

--- src/app/components/MixerCard/MixerCard.tsx
+++ src/app/components/MixerCard/MixerCard.tsx
@@ -5,12 +5,9 @@ import style from './MixerCard.module.css';
 export default function MixerCard(): JSX.Element {
   return (
     <section className={style.cardWrapper}>
-      <div>
-        <Typography size="m" color="dark" children="Moog Matriarch" />
-      </div>
-      <div>
-        <Typography size="m" color="red" children="19/20" />
-      </div>
+      <Typography size="m" color="dark" children="Moog Matriarch" />
+
+      <Typography size="m" color="red" children="19/20" />
     </section>
   );
 }
