ca91095e37dc92379bf78e3550ecb47b3c10b1aa
Seborider <mayer.sebobo@gmail.com>
2021-09-13 15:03:52 +0200

    ✨  Create GearCardEdit component

---
+++ src/app/components/GearCardEdit/GearCardEdit.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import GearCardEdit from './GearCardEdit';
+
+export default {
+  title: 'Component/GearCardEdit',
+  component: GearCardEdit,
+};
+
+export const Edit = (): JSX.Element => <GearCardEdit />;
