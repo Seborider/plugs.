37f53891da3f9014823dea3a8e6ed95bf08ad3ca
Seborider <mayer.sebobo@gmail.com>
2021-09-14 17:45:52 +0200

    ✨  Add GearCard component

---
+++ src/app/components/GearCard/GearCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import GearCardHeader from '../GearCardHeader/GearCardHeader';
+import GearCardButtonSection from '../GearCardEdit/GearCardEdit';
+import GearCardConnection from '../GearCardConnection/GearCardConnection';
+import style from './GearCard.module.css';
+
+type GearCardProps = {
+  iconType: 'Synth' | 'Effect' | 'Drum';
+  name: string;
+  connections: { channel: string; connection: string }[];
+};
+
+export default function GearCard({
+  iconType,
+  name,
+  connections,
+}: GearCardProps): JSX.Element {
+  return (
+    <section className={style.card}>
+      <div className={style.cardWrapper}>
+        <div className={style.header}>
+          <GearCardHeader iconType={iconType} name={name} />
+        </div>
+        <div className={style.cardBody}>
+          {connections.map((connection) => (
+            <GearCardConnection
+              connection={connection.connection}
+              channel={connection.channel}
+            />
+          ))}
+        </div>
+        <GearCardButtonSection />
+      </div>
+    </section>
+  );
+}
