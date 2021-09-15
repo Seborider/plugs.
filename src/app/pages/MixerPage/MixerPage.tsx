05030d1c6741bc9e4ec07cdf93833be9dac1e3c3
Seborider <mayer.sebobo@gmail.com>
2021-09-15 15:25:23 +0200

    🚚  Move MixerPage in correct folder

--- src/app/pages/MixerPage/MixerPage.tsx
+++ src/app/pages/MixerPage/MixerPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from '../components/Header/Header';
-import MixerCard from '../components/MixerCard/MixerCard';
-import NavBar from '../components/NavBar/NavBar';
-import style from './MixerPage.module.css';
+import Header from '../../components/Header/Header';
+import MixerCard from '../../components/MixerCard/MixerCard';
+import NavBar from '../../components/NavBar/NavBar';
+import style from './../MixerPage/MixerPage.module.css';
 
 const MixerCardMockData = [
   { gearname: 'Moog Matriarch', channel: '19/20' },
