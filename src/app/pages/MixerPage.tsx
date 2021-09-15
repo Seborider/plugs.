0304c2ae0e07838726b7aef48af4c6f24ab3431e
Seborider <mayer.sebobo@gmail.com>
2021-09-15 12:53:43 +0200

    ✨  Create MixerPage including MockData

---
+++ src/app/pages/MixerPage.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import MixerCard from '../components/MixerCard/MixerCard';
+import NavBar from '../components/NavBar/NavBar';
+import style from './MixerPage.module.css';
+
+const MixerCardMockData = [
+  { gearname: 'Moog Matriarch', channel: '19/20' },
+  { gearname: 'SH101', channel: '8' },
+  { gearname: '808', channel: '4' },
+  { gearname: 'Moog Matriarch', channel: '19/20' },
+  { gearname: 'SH101', channel: '8' },
+  { gearname: '808', channel: '4' },
+  { gearname: 'Moog Matriarch', channel: '19/20' },
+  { gearname: 'SH101', channel: '8' },
+  { gearname: '808', channel: '4' },
+  { gearname: 'Moog Matriarch', channel: '19/20' },
+  { gearname: 'SH101', channel: '8' },
+  { gearname: '808', channel: '4' },
+];
+
+export default function MixerPage(): JSX.Element {
+  return (
+    <div className={style.pageContainer}>
+      <Header text="Mixer" className={style.header} />
+
+      <main className={style.main}>
+        {MixerCardMockData.map((item) => (
+          <MixerCard gearname={item.gearname} channel={item.channel} />
+        ))}
+      </main>
+      <nav>
+        <NavBar selected="Mixer" />
+      </nav>
+    </div>
+  );
+}
