ca165aad712f8f0854cab2fd70946b6036a9fe36
Seborider <mayer.sebobo@gmail.com>
2021-09-16 14:35:08 +0200

    ✨  Create SaveButton

---
+++ src/app/components/SaveButton/SaveButton.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SaveButton from './SaveButton';
+
+export default {
+  title: 'Component/SaveButton',
+  component: SaveButton,
+};
+
+export const Save = (): JSX.Element => <SaveButton text="Save" />;
