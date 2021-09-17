48e62f33df1ef0fa346079fc90345677b809f1fd
Seborider <mayer.sebobo@gmail.com>
2021-09-17 16:48:41 +0200

    ✨  Implement functionality to SaveButton

--- src/app/components/SaveButton/SaveButton.tsx
+++ src/app/components/SaveButton/SaveButton.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 import SaveIcon from '../assets/SaveIcon';
 import Typography from '../Typography/Typography';
 import style from './SaveButton.module.css';
 
 export type SaveButtonProps = {
   text: string;
+  type: 'submit';
 };
 
-export default function SaveButton({ text }: SaveButtonProps): JSX.Element {
+export default function SaveButton({
+  text,
+  type,
+}: SaveButtonProps): JSX.Element {
   return (
     <div className={style.saveWrapper}>
       <Typography size="s" color="light">
         {text}
       </Typography>
       <div className={style.line}></div>
-      <button className={style.button}>
+      {/* <Link to="/"> */}
+      <button className={style.button} type={type}>
         <SaveIcon fill={'var(--primary-light)'} />
       </button>
+      {/* </Link> */}
     </div>
   );
 }
