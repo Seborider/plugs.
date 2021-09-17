4176a7af3425af14f8975670f04b02f2e8161eac
Seborider <mayer.sebobo@gmail.com>
2021-09-17 14:32:54 +0200

    ♻️  Add types for functionality

--- src/app/components/InputBar/InputBar.tsx
+++ src/app/components/InputBar/InputBar.tsx
@@ -3,15 +3,23 @@ import styles from './InputBar.module.css';
 
 export type InputBarProps = {
   type?: 'search' | 'add';
+  value?: string;
+  onChange: (value: string) => void;
 };
 
-export default function InputBar({ type }: InputBarProps): JSX.Element {
+export default function InputBar({
+  type,
+  value,
+  onChange,
+}: InputBarProps): JSX.Element {
   return (
     <form>
       <input
         type={type === 'search' ? 'search' : 'text'}
         placeholder={type === 'search' ? 'search' : 'add gear'}
         className={styles.input}
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
       />
     </form>
   );
