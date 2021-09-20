16f3c0fd130da47452904369b231eb9989f59087
Seborider <mayer.sebobo@gmail.com>
2021-09-20 15:29:42 +0200

    ✨  Create connectDatabase function

---
+++ src/app/utils/database.ts
@@ -0,0 +1,18 @@
+import type { Collection } from 'mongodb';
+import { MongoClient } from 'mongodb';
+import type { Gear } from '../../types';
+
+let client: MongoClient;
+
+export async function connectDatabase(url: string): Promise<void> {
+  client = new MongoClient(url);
+  await client.connect();
+}
+
+export function getCollection<T>(name: string): Collection<T> {
+  return client.db().collection<T>(name);
+}
+
+export function getGearCollection(): Collection<Gear> {
+  return getCollection<Gear>('gear');
+}
