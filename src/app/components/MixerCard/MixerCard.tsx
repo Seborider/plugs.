bbea477a9bc52996aecb1c5fc9c02035db403d34
Seborider <mayer.sebobo@gmail.com>
2021-09-21 17:21:28 +0200

    ♻️  Change naming

--- src/app/components/MixerCard/MixerCard.tsx
+++ src/app/components/MixerCard/MixerCard.tsx
@@ -3,19 +3,19 @@ import Typography from '../Typography/Typography';
 import style from './MixerCard.module.css';
 
 type MixerCardProps = {
-  gearname: string;
-  channel: string;
+  name: string;
+  connection: string;
 };
 
 export default function MixerCard({
-  gearname,
-  channel,
+  name,
+  connection,
 }: MixerCardProps): JSX.Element {
   return (
     <section className={style.cardWrapper}>
-      <Typography size="m" color="dark" children={gearname} />
+      <Typography size="m" color="dark" children={name} />
 
-      <Typography size="m" color="red" children={channel} />
+      <Typography size="m" color="red" children={connection} />
     </section>
   );
 }
