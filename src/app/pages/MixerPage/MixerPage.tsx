aee035f6290396cb598f7de086f6a7b4cdc528e6
Seborider <mayer.sebobo@gmail.com>
2021-09-21 17:22:32 +0200

    ✨  Add fetch function for MixerCards

--- src/app/pages/MixerPage/MixerPage.tsx
+++ src/app/pages/MixerPage/MixerPage.tsx
@@ -1,32 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import type { MixerGear } from '../../../types';
 import Header from '../../components/Header/Header';
 import MixerCard from '../../components/MixerCard/MixerCard';
 import NavBar from '../../components/NavBar/NavBar';
 import style from './../MixerPage/MixerPage.module.css';
 
-const MixerCardMockData = [
-  { gearname: 'Moog Matriarch', channel: '19/20' },
-  { gearname: 'SH101', channel: '8' },
-  { gearname: '808', channel: '4' },
-  { gearname: 'Moog Matriarch', channel: '19/20' },
-  { gearname: 'SH101', channel: '8' },
-  { gearname: '808', channel: '4' },
-  { gearname: 'Moog Matriarch', channel: '19/20' },
-  { gearname: 'SH101', channel: '8' },
-  { gearname: '808', channel: '4' },
-  { gearname: 'Moog Matriarch', channel: '19/20' },
-  { gearname: 'SH101', channel: '8' },
-  { gearname: '808', channel: '4' },
-];
+// const MixerCardMockData = [
+//   { gearname: 'Moog Matriarch', channel: '19/20' },
+//   { gearname: 'SH101', channel: '8' },
+//   { gearname: '808', channel: '4' },
+//   { gearname: 'Moog Matriarch', channel: '19/20' },
+//   { gearname: 'SH101', channel: '8' },
+//   { gearname: '808', channel: '4' },
+//   { gearname: 'Moog Matriarch', channel: '19/20' },
+//   { gearname: 'SH101', channel: '8' },
+//   { gearname: '808', channel: '4' },
+//   { gearname: 'Moog Matriarch', channel: '19/20' },
+//   { gearname: 'SH101', channel: '8' },
+//   { gearname: '808', channel: '4' },
+// ];
 
 export default function MixerPage(): JSX.Element {
+  const [gear, setGear] = useState<MixerGear[]>([]);
+
+  async function fetchGear() {
+    const response = await fetch('/api/gear', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const gear = await response.json();
+    setGear(gear);
+  }
+
+  useEffect(() => {
+    fetchGear();
+  }, []);
+
   return (
     <div className={style.pageContainer}>
       <Header text="Mixer" className={style.header} />
 
       <main className={style.main}>
-        {MixerCardMockData.map((item) => (
-          <MixerCard gearname={item.gearname} channel={item.channel} />
+        {gear.map((item) => (
+          <MixerCard
+            name={item.name}
+            connection={item.connections[0]?.channel}
+          />
         ))}
       </main>
       <nav>
