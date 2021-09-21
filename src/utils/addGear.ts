8a75d7312b24485dc8fc3535457c3ce8f8678648
Seborider <mayer.sebobo@gmail.com>
2021-09-21 14:49:18 +0200

    🔥  Delete addGear

--- src/utils/addGear.ts
+++
@@ -1,7 +0,0 @@
-import type { Gear } from '../types';
-import { getGearCollection } from './database';
-
-export async function addGear(gear: Gear): Promise<void> {
-  const items = getGearCollection();
-  items.insertOne(gear);
-}
