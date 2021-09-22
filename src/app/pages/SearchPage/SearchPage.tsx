2e2c514f3713f509311ba4600275a73a95eb5612
Seborider <mayer.sebobo@gmail.com>
2021-09-22 18:34:19 +0200

    ✨  Add handleSubmit function

--- src/app/pages/SearchPage/SearchPage.tsx
+++ src/app/pages/SearchPage/SearchPage.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import GearCard from '../../components/GearCard/GearCard';
 import NavBar from '../../components/NavBar/NavBar';
 import style from './SearchPage.module.css';
+import type { Gear } from '../../../types';
 
 type MockData = {
   iconType: 'Synth' | 'Effect' | 'Drum';
@@ -23,6 +24,23 @@ const GearCardMockData: MockData[] = [
 ];
 
 export default function SearchPage(): JSX.Element {
+  const [results, setResults] = useState<Gear[]>([]);
+
+  async function handleSubmit(
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+    const response = await fetch(`/api/gear/${name}`);
+    const result = await response.json();
+    setResults(result);
+  }
+
+  async function deleteGear(name: string) {
+    await fetch(`/api/gear/${name}`, {
+      method: 'DELETE',
+    });
+  }
+
   return (
     <div className={style.pageContainer}>
       <Header
@@ -31,13 +49,17 @@ export default function SearchPage(): JSX.Element {
         type="search"
         className={style.header}
         onChange={console.log}
+        onSubmit={handleSubmit}
       />
+
       <main className={style.main}>
-        {GearCardMockData.map((item) => (
+        {results.map((item) => (
           <GearCard
             iconType={item.iconType}
             name={item.name}
             connections={item.connections}
+            onDeleteClick={() => deleteGear(item.name)}
+            // key={item._id}
           />
         ))}
       </main>
