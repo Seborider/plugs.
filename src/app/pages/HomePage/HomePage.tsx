d5b417163f4008b402d101a241b31fbb985b8d6c
Seborider <mayer.sebobo@gmail.com>
2021-09-24 14:40:58 +0200

    ♻️  Implement useFetch hook

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -1,48 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../../components/Header/Header';
 import GearCard from '../../components/GearCard/GearCard';
 import NavBar from '../../components/NavBar/NavBar';
 import type { Gear } from '../../../types';
 import style from './HomePage.module.css';
+import useFetch from '../../hooks/useFetch';
 
 export default function HomePage(): JSX.Element {
-  const [gear, setGear] = useState<Gear[]>([]);
-
-  async function fetchGear() {
-    const response = await fetch('/api/gear', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const gear = await response.json();
-    setGear(gear);
-  }
-
+  const { data: gear, refetch } = useFetch<Gear[]>('/api/gear');
   async function deleteGear(name: string) {
     await fetch(`/api/gear/${name}`, {
       method: 'DELETE',
     });
-    fetchGear();
+    refetch();
   }
-
-  useEffect(() => {
-    fetchGear();
-  }, []);
 
   return (
     <div className={style.pageContainer}>
       <Header withLogo withBurgerButton className={style.header} />
 
       <main className={style.main}>
-        {gear.map((item) => (
-          <GearCard
-            iconType={item.iconType}
-            name={item.name}
-            connections={item.connections}
-            onDeleteClick={() => deleteGear(item.name)}
-          />
-        ))}
+        {gear &&
+          gear.map((item) => (
+            <GearCard
+              iconType={item.iconType}
+              name={item.name}
+              connections={item.connections}
+              onDeleteClick={() => deleteGear(item.name)}
+            />
+          ))}
       </main>
       <nav>
         <NavBar selected="Home" />
