a5601d1e5d039bfeef99e3f0d4b4639ef8dee7a2
Seborider <mayer.sebobo@gmail.com>
2021-09-28 13:38:50 +0200

    ✨  Add modal on burgerButton

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -5,8 +5,13 @@ import NavBar from '../../components/NavBar/NavBar';
 import type { Gear } from '../../../types';
 import style from './HomePage.module.css';
 import useFetch from '../../hooks/useFetch';
+import { useState } from 'react';
+import Typography from '../../components/Typography/Typography';
+import BurgerButton from '../../components/BurgerButton/BurgerButton';
 
 export default function HomePage(): JSX.Element {
+  const [isModalOpen, setModal] = useState(false);
+
   const { data: gear, refetch } = useFetch<Gear[]>('/api/gear');
   async function deleteGear(name: string) {
     await fetch(`/api/gear/${name}`, {
@@ -15,9 +20,18 @@ export default function HomePage(): JSX.Element {
     refetch();
   }
 
+  function modalClick() {
+    setModal(!isModalOpen);
+  }
+
   return (
     <div className={style.pageContainer}>
-      <Header withLogo withBurgerButton className={style.header} />
+      <Header
+        withLogo
+        withBurgerButton
+        className={style.header}
+        onClick={() => modalClick()}
+      />
 
       <main className={style.main}>
         {gear &&
@@ -31,6 +45,68 @@ export default function HomePage(): JSX.Element {
             />
           ))}
       </main>
+      <section>
+        {isModalOpen === true && (
+          <div className={style.modal}>
+            <article>
+              <header className={style.modalHeader}>
+                <div className={style.burgerButton}>
+                  <BurgerButton onClick={modalClick} />
+                </div>
+                <Typography
+                  size="m"
+                  color="white"
+                  children="About"
+                  className={style.modalHeader}
+                />
+                <div className={style.line}></div>
+              </header>
+              <div className={style.modalText}>
+                <Typography
+                  size="s"
+                  color="light"
+                  children={
+                    <>
+                      <p>
+                        Plugs. lets you organize your home studio: With plugs.
+                        finding what is where in your home studio was never
+                        easier!{' '}
+                      </p>
+                      <p>
+                        plugs. lets you define, where you plugged what, which
+                        cable goes where, and which output is connected to which
+                        input, and vice versa.
+                      </p>
+                      <p>
+                        Just look for your instrument itself, edit your
+                        everchanging and expanding gear setup on the go, or see
+                        your mixer for a quick overview.
+                      </p>
+                    </>
+                  }
+                />
+              </div>
+            </article>
+            <article>
+              <header className={style.modalHeader}>
+                <Typography size="m" color="white" children="Contact" />
+                <div className={style.line}></div>
+              </header>
+              <div className={style.modalText}>
+                <Typography
+                  size="s"
+                  color="light"
+                  children={
+                    <a href="mailto:mayer.sebobo@gmail.com?subject=plugs">
+                      mail@plugs.com
+                    </a>
+                  }
+                />
+              </div>
+            </article>
+          </div>
+        )}
+      </section>
       <nav>
         <NavBar selected="Home" />
       </nav>
