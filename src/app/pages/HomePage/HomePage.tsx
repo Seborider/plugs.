068954e1b58bf8d18b7d5dcbca379db28a8af852
Seborider <mayer.sebobo@gmail.com>
2021-09-29 13:40:19 +0200

    ✨  Add deleteModal

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import GearCard from '../../components/GearCard/GearCard';
 import NavBar from '../../components/NavBar/NavBar';
 import type { Gear } from '../../../types';
 import style from './HomePage.module.css';
 import useFetch from '../../hooks/useFetch';
+import Typography from '../../components/Typography/Typography';
 
 export default function HomePage(): JSX.Element {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [deleteItem, setDeleteItem] = useState<Gear>();
+
   const { data: gear, refetch } = useFetch<Gear[]>('/api/gear');
   async function deleteGear(name: string) {
     await fetch(`/api/gear/${name}`, {
       method: 'DELETE',
     });
     refetch();
+    setShowDeleteModal(false);
   }
 
   return (
@@ -26,14 +31,37 @@ export default function HomePage(): JSX.Element {
               iconType={item.iconType}
               name={item.name}
               connections={item.connections}
-              onDeleteClick={() => deleteGear(item.name)}
+              onDeleteClick={() => (
+                setDeleteItem(item), setShowDeleteModal(true)
+              )}
               key={item.name}
             />
           ))}
+        {showDeleteModal && (
+          <section className={style.deleteModal}>
+            <Typography size="s" color="white">
+              Delete gear?
+            </Typography>
+            <div>
+              <button onClick={() => setShowDeleteModal(false)}>
+                <Typography size="s" color="dark">
+                  No
+                </Typography>
+              </button>
+              <button onClick={() => deleteGear(deleteItem.name)}>
+                <Typography size="s" color="dark">
+                  Yes
+                </Typography>
+              </button>
+            </div>
+          </section>
+        )}
       </main>
+
       <nav>
         <NavBar selected="Home" />
       </nav>
     </div>
   );
 }
+// () => deleteGear(item.name);
